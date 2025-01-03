f775911f070dbe08f8c868be95863a8ee352d38e
penguindisc <n84066@gmail.com>
2025-01-03 16:19:34 +0900

    6.1이후 지원되지 않는 구문 수정

--- src/main/java/penguindisco/loginproject/configuration/SecurityConfig.java
+++ src/main/java/penguindisco/loginproject/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import penguindisco.loginproject.service.CustomOAuth2UserService;
 
 @Configuration
@@ -20,7 +21,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository())
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login/**", "/oauth2/**", "/register").permitAll()
                         .anyRequest().authenticated()
@@ -31,8 +34,9 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth -> oauth
                         .loginPage("/login")
-                        .userInfoEndpoint()
-                        .userService(customOAuth2UserService)
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
                 );
         return http.build();
     }
