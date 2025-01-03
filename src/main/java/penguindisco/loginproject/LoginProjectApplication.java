1511f86e1c1b66feb8f0f801df0189c4778c1945
penguindisc <n84066@gmail.com>
2025-01-03 14:22:29 +0900

    Initial commit

---
+++ src/main/java/penguindisco/loginproject/LoginProjectApplication.java
@@ -0,0 +1,13 @@
+package penguindisco.loginproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginProjectApplication.class, args);
+    }
+
+}
