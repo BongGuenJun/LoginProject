1b4571b96d50870f8e2d1576edcc4fa543625933
penguindisc <n84066@gmail.com>
2025-01-06 17:21:13 +0900

    securityConfig에서 문제 발생

--- src/main/java/penguindisco/loginproject/configuration/SecurityConfig.java
+++ src/main/java/penguindisco/loginproject/configuration/SecurityConfig.java
@@ -2,51 +2,47 @@ package penguindisco.loginproject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import penguindisco.loginproject.service.CustomOAuth2UserService;
 
 @Configuration
 public class SecurityConfig {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
-
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-        this.customOAuth2UserService = customOAuth2UserService;
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository())
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/userJoin", "/loginPage", "/oauth2/**", "/register", "/oauth2.0/*").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage("/loginPage")
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .oauth2Login(oauth -> oauth
-                        .loginPage("/loginPage")
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(customOAuth2UserService)
-                        )
-                )
-        .sessionManagement(session -> session
-                .sessionFixation().migrateSession() // 세션 고정 공격 방지
-                .maximumSessions(1) // 최대 세션 수
-                .maxSessionsPreventsLogin(true) // 최대 세션 수 초과 시 로그인 방지
-        );
-        return http.build();
-    }
-
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//
+//    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+//        this.customOAuth2UserService = customOAuth2UserService;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf
+//                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository())
+//                )
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/", "/userJoin", "/loginPage", "/oauth2/**", "/register", "/oauth2.0/*", "/static/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .oauth2Login(oauth -> oauth
+//                        .loginPage("/loginPage")
+//                        .userInfoEndpoint(userInfo -> userInfo
+//                                .userService(customOAuth2UserService)
+//                        )
+//                )
+//        .sessionManagement(session -> session
+//                .sessionFixation().migrateSession() // 세션 고정 공격 방지
+//                .maximumSessions(1) // 최대 세션 수
+//                .maxSessionsPreventsLogin(true) // 최대 세션 수 초과 시 로그인 방지
+//        );
+//        return http.build();
+//    }
+//
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
